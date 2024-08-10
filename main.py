b3066f7023800fac206c31bd40300a60c795e6bb
yurazver <yurazver0@gmail.com>
2024-08-10 07:11:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+def calculate_structure_sum(data):
+    total_sum = 0
+
+    if isinstance(data, dict):
+
+        for key, value in data.items():
+            total_sum += calculate_structure_sum(key)
+            total_sum += calculate_structure_sum(value)
+
+    elif isinstance(data, (list, tuple)):
+
+        for item in data:
+            total_sum += calculate_structure_sum(item)
+
+    elif isinstance(data, str):
+
+        total_sum += len(data)
+
+    elif isinstance(data, (int, float)):
+
+        total_sum += data
+
+    return total_sum
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
