96ddb4eab75a03acd1e7b9eb9d2183808feb5153
yurazver <yurazver0@gmail.com>
2024-08-11 17:04:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 def calculate_structure_sum(data):
     total_sum = 0
 
-    if isinstance(data, dict):
+    if isinstance(data, int):
+
+        return data
+    elif isinstance(data, str):
+
+        return len(data)
+    elif isinstance(data, (list, tuple, set)):
+
+        for item in data:
+            total_sum += calculate_structure_sum(item)
+    elif isinstance(data, dict):
 
         for key, value in data.items():
             total_sum += calculate_structure_sum(key)
             total_sum += calculate_structure_sum(value)
-
-    elif isinstance(data, (list, tuple)):
-
-        for item in data:
-            total_sum += calculate_structure_sum(item)
-
-    elif isinstance(data, str):
-
-        total_sum += len(data)
-
-    elif isinstance(data, (int, float)):
-
-        total_sum += data
 
     return total_sum
 
